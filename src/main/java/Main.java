82018afe8b418e081301003196cb0814003ecb5c
- <->
2024-05-23 23:41:56 +0500

    fff

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,32 @@
 import java.util.*;
 
 public class Main {
+
+    public static void main(String[] args) {
+        Map<String, List<String>> adjacencyList = new HashMap<>();
+        adjacencyList.put("A", Arrays.asList("C", "B", "D"));
+        adjacencyList.put("B", Arrays.asList("A", "C", "E", "G"));
+        adjacencyList.put("C", Arrays.asList("A", "B", "D"));
+        adjacencyList.put("D", Arrays.asList("C", "A"));
+        adjacencyList.put("E", Arrays.asList("G", "F", "B"));
+        adjacencyList.put("F", Arrays.asList("G", "E"));
+        adjacencyList.put("G", Arrays.asList("F", "B", "E"));
+
+        Main graph = new Main(adjacencyList);
+
+        List<String> dfsOrder = graph.DepthFirstSearch("A");
+        List<String> bfsOrder = graph.BreadthFirstSearch("A");
+
+        System.out.println("DFS: " + dfsOrder);
+        System.out.println("BFS: " + bfsOrder);
+    }
     private Map<String, List<String>> adjacencyList;
 
         public Main(Map<String, List<String>> adjacencyList) {
         this.adjacencyList = adjacencyList;
     }
 
-    public List<String> dfs(String start) {
+    public List<String> DepthFirstSearch(String start) {
         Stack<String> stack = new Stack<>();
         List<String> result = new ArrayList<>();
         Set<String> visited = new HashSet<>();
@@ -31,7 +50,7 @@ public class Main {
         return result;
     }
 
-    public List<String> bfs(String start) {
+    public List<String> BreadthFirstSearch(String start) {
         Set<String> visited = new HashSet<>();
         List<String> result = new ArrayList<>();
         Queue<String> queue = new LinkedList<>();
@@ -54,22 +73,5 @@ public class Main {
         return result;
     }
 
-    public static void main(String[] args) {
-        Map<String, List<String>> adjacencyList = new HashMap<>();
-        adjacencyList.put("A", Arrays.asList("C", "B", "D"));
-        adjacencyList.put("B", Arrays.asList("A", "C", "E", "G"));
-        adjacencyList.put("C", Arrays.asList("A", "B", "D"));
-        adjacencyList.put("D", Arrays.asList("C", "A"));
-        adjacencyList.put("E", Arrays.asList("G", "F", "B"));
-        adjacencyList.put("F", Arrays.asList("G", "E"));
-        adjacencyList.put("G", Arrays.asList("F", "B", "E"));
 
-        Main graph = new Main(adjacencyList);
-
-        List<String> dfsOrder = graph.dfs("A");
-        List<String> bfsOrder = graph.bfs("A");
-
-        System.out.println("DFS: " + dfsOrder);
-        System.out.println("BFS: " + bfsOrder);
-    }
 }
